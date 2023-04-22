0b5c06d8ac0fa50bbeac301eb5f0996454b9b651
EstevanKov <franciscoesteban15rdz@gmail.com>
2023-04-22 06:48:13 -0600

    graficas listas, solo falta aplicar el filtro y pulir detalles

--- routes/index.js
+++ routes/index.js
@@ -10,43 +10,50 @@ const sensoresConroller = require("../controllers/sensores");
 const bitacorasController = require("../controllers/bitacoras");
 const reportesController = require("../controllers/reportes");
 const graficasController = require("../controllers/general");
+const graficasAireController = require("../controllers/generalAire");
+const graficasSueloController = require("../controllers/generalSuelo");
+const graficasAguaController = require("../controllers/generalAgua");
 
 //Areas
-const areasController = require ('../controllers/areas');
+const areasController = require('../controllers/areas');
 
-module.exports = () =>{
-    router.get("/", indexController.index);
+module.exports = () => {
+  router.get("/", indexController.index);
 
-    router.get("/interfaces", interfacesController.index);
-    router.post("/interfaces", interfacesController.add);
-    router.get("/interfaces/:id", interfacesController.show);
+  router.get("/interfaces", interfacesController.index);
+  router.post("/interfaces", interfacesController.add);
+  router.get("/interfaces/:id", interfacesController.show);
 
-    router.get("/modules", modulosController.index);
-    router.post("/modules", modulosController.add);
-    router.get("/modules/:id", modulosController.show)
+  router.get("/modules", modulosController.index);
+  router.post("/modules", modulosController.add);
+  router.get("/modules/:id", modulosController.show)
 
-    router.get("/sections", seccionesController.index);
-    router.post("/sections", seccionesController.add);
-    router.get("/sections/:id", seccionesController.show);
-    router.put("/sections/:id", seccionesController.update);
+  router.get("/sections", seccionesController.index);
+  router.post("/sections", seccionesController.add);
+  router.get("/sections/:id", seccionesController.show);
+  router.put("/sections/:id", seccionesController.update);
 
-    router.get("/sensors", sensoresConroller.index);
-    router.post("/sensors", sensoresConroller.add);
-    router.get("/sensors/:id", sensoresConroller.show);
-    router.put("/sensors/:id", sensoresConroller.update);
+  router.get("/sensors", sensoresConroller.index);
+  router.post("/sensors", sensoresConroller.add);
+  router.get("/sensors/:id", sensoresConroller.show);
+  router.put("/sensors/:id", sensoresConroller.update);
 
-    router.get("/log", bitacorasController.index);
-    router.post("/log", bitacorasController.add);
-    router.get("/log/:id", bitacorasController.show);
+  router.get("/log", bitacorasController.index);
+  router.post("/log", bitacorasController.add);
+  router.get("/log/:id", bitacorasController.show);
 
-    router.get("/reportes", reportesController.index);
+  router.get("/reportes", reportesController.index);
 
-    router.get("/general",graficasController.index);
+  //graficas generales
+  router.get("/general", graficasController.index);
+  router.get("/agua", graficasAguaController.index);
+  router.get("/suelo", graficasSueloController.index);
+  router.get("/aire", graficasAireController.index);
 
-      //mostrarAreas
+  //mostrarAreas
 
-  router.get('/areas',areasController.index);
+  router.get('/areas', areasController.index);
 
 
-    return router;
+  return router;
 };
