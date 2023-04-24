64c6fe9b6c2959d9db5850bc2e73f16cb09870f4
Diego SHS <diegoawesome2017@outlook.com>
2023-04-24 13:45:45 -0500

    cambios en params id en actualizarestdo.js

--- controllers/actualizarEstdo.js
+++ controllers/actualizarEstdo.js
@@ -4,8 +4,8 @@ const dbo = require("../models/connection")
 
 exports.update = async (req, res) => {
     try {
-
-        if (!req.params.id || req.params.id.length !== 24) {
+        const { params:{id},body:{state} } = req
+        if (!id || id.length !== 24) {
             return res.status(400).json({
                 message: 'ID inválido',
             });
@@ -13,15 +13,14 @@ exports.update = async (req, res) => {
         const db = dbo.getDb();
         const sensors = await db.collection("sensors");
 
-        const { state } = req.body;
-
-        const query = { _id: new ObjectId(req.params.id) };
+        const query = { _id: new ObjectId(id) };
         const updates = {
             $set: { state }
         };
         const result = await sensors.updateOne(query, updates);
         res.json({
-            message: 'El estado se actualizó correctamente'
+            message: 'El estado se actualizó correctamente',
+            result,
         });
     } catch (error) {
         console.error(error)
